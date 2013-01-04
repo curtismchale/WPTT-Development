fe216bcbd11432dc1518742faead57b34a88afa3
curtismchale <curtis@curtismchale.ca>
2013-01-04 00:28:15 -0800

    - logging emails
    This is a really rough working copy. I don't like the current file names, or exactly how I this is happening but it is what I have

--- sfn-dev.php
+++ sfn-dev.php
@@ -29,26 +29,65 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  * @todo add logging of all emails locally
  */
 
-/*
- * Produces print_r inside <pre> limited to development users
- *
- * @param string $data The variable we want to print
- * @uses get_the_author_meta
- * @uses current_user_can
- * @ueses in_array
- */
-function pr( $data ) {
+/* WP Logging to handle the logging */
+require_once( plugin_dir_path( __FILE__ ) . '/lib/wp-logging/WP_Logging.php' );
 
-    global $current_user;
+class WPTT_Dev{
 
-    $validemails = array( 'curtis@curtismchale.ca', 'curtis.mchale@gmail.com' );
-
-    $useremail = get_the_author_meta( 'user_email', $current_user->ID );
-
-    if( WP_DEBUG || current_user_can( 'activate_plugins' ) || in_array( $useremail, $validemails )  ){
-        echo '<pre>';
-        print_r( $data );
-        echo '</pre>';
+    function __construct(){
+        add_filter( 'wp_logging_post_type_args', array( $this, 'change_logging_params' ), 10, 1 );
     }
 
+    /**
+     * Changing the logging arguments for the post type so that we have a basic admin
+     *
+     * @param   array   $params         The original parameters
+     * @return  array                   Our modified parameters
+     *
+     * @since   0.01
+     * @author  WP Theme Tutorial, Curtis McHale
+     */
+    public function change_logging_params( $params ){
+
+        $params['public'] = true;
+        $params['show_in_nav_menus'] = false;
+        $params['show_in_menu'] = 'tools.php';
+
+        return $params;
+
+    } // change_logging_params
+
+} // wptt_dev
+
+$wptt_dev = new WPTT_Dev();
+
+if( site_url() === WPTT_DEV ){
+    /* log the emails - requires wp logging */
+    require_once( plugin_dir_path( __FILE__ ) . '/class.log-email.php' );
 }
+
+if( ! function_exists( 'pr' ) ){
+    /*
+     * Produces print_r inside <pre> limited to development users
+     *
+     * @param string $data The variable we want to print
+     * @uses get_the_author_meta
+     * @uses current_user_can
+     * @ueses in_array
+     */
+    function pr( $data ) {
+
+        global $current_user;
+
+        $validemails = array( 'curtis@curtismchale.ca', 'curtis.mchale@gmail.com' );
+
+        $useremail = get_the_author_meta( 'user_email', $current_user->ID );
+
+        if( WP_DEBUG || current_user_can( 'activate_plugins' ) || in_array( $useremail, $validemails )  ){
+            echo '<pre>';
+            print_r( $data );
+            echo '</pre>';
+        }
+
+    }
+} // function_exists( pr )
