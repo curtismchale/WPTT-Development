ae92e7b973e2c98f84afa65527736e99f3d554f5
curtismchale <curtis@curtismchale.ca>
2013-01-19 15:17:27 -0800

    WP coding standards

--- pluggable.php
+++ pluggable.php
@@ -12,24 +12,24 @@
  * Since we take over I'm logging all emails with WP_Logging.
  */
 if( function_exists( 'wp_mail' ) ){
-  function wp_mail( $to, $subject, $message, $headers = '', $attachments = array() ) {
+	function wp_mail( $to, $subject, $message, $headers = '', $attachments = array() ) {
 
-      $log_data = array(
-          'post_title'    => 'Email: '. $subject,
-          'post_content'  => wp_kses_post( $message ),
-          'log_type'      => 'event',
-      );
+		$log_data = array(
+			'post_title'    => 'Email: '. $subject,
+			'post_content'  => wp_kses_post( $message ),
+			'log_type'      => 'event',
+		);
 
-      // meta
-      $log_meta = array(
-          'to_email'      => $to,
-          'headers'       => $headers,
-          'attachments'   => $attachments,
-          'date_time'     => time(),
-          'raw_message'   => $message,
-      );
+		// meta
+		$log_meta = array(
+			'to_email'      => $to,
+			'headers'       => $headers,
+			'attachments'   => $attachments,
+			'date_time'     => time(),
+			'raw_message'   => $message,
+		);
 
-      $log_entry = WP_Logging::insert_log( $log_data, $log_meta );
+		$log_entry = WP_Logging::insert_log( $log_data, $log_meta );
 
-  } // wp_mail
+	} // wp_mail
 } // function_exists
