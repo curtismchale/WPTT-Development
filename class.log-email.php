706e69525184cc54a2983cc1791bb82dccf6a5d9
curtismchale <curtis@curtismchale.ca>
2013-01-19 14:18:02 -0800

    - wrapping the function in function_exists
    On activation the function will be already defined so we need to wrap it
    in a function_exists check or we get activation errors.

--- class.log-email.php
+++ class.log-email.php
@@ -11,23 +11,25 @@
  * wp_mail is a pluggable function so we are just declaring it and thus taking over for it.
  * Since we take over I'm logging all emails with WP_Logging.
  */
-function wp_mail( $to, $subject, $message, $headers = '', $attachments = array() ) {
+if( function_exists( 'wp_mail' ) ){
+  function wp_mail( $to, $subject, $message, $headers = '', $attachments = array() ) {
 
-    $log_data = array(
-        'post_title'    => 'Email: '. $subject,
-        'post_content'  => wp_kses_post( $message ),
-        'log_type'      => 'event',
-    );
+      $log_data = array(
+          'post_title'    => 'Email: '. $subject,
+          'post_content'  => wp_kses_post( $message ),
+          'log_type'      => 'event',
+      );
 
-    // meta
-    $log_meta = array(
-        'to_email'      => $to,
-        'headers'       => $headers,
-        'attachments'   => $attachments,
-        'date_time'     => time(),
-        'raw_message'   => $message,
-    );
+      // meta
+      $log_meta = array(
+          'to_email'      => $to,
+          'headers'       => $headers,
+          'attachments'   => $attachments,
+          'date_time'     => time(),
+          'raw_message'   => $message,
+      );
 
-    $log_entry = WP_Logging::insert_log( $log_data, $log_meta );
+      $log_entry = WP_Logging::insert_log( $log_data, $log_meta );
 
-} // wp_mail
+  } // wp_mail
+} // function_exists
